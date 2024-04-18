54249fdd97a95881301df817d2f457b1d078df09
AyeChanHtunNaing <ayechanhtunnaing@icloud.com>
2024-04-18 22:25:33 +0630

    feat : draft html

--- react-todo-demo/react-todos/src/App.js
+++ react-todo-demo/react-todos/src/App.js
@@ -3,22 +3,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>First React App</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <div>
+          Your Todo's
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Description</th>
+                <th scope='col'>Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope='row'>1</th>
+                <td>Code Review</td>
+                <td>Pika</td>
+              </tr>
+              <tr>
+                <th scope='row'>2</th>
+                <td>Develop Frontend App</td>
+                <td>Chuu</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
