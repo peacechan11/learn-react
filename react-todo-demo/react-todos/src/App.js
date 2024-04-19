2e169d63c229a7c0067745241be01659f3aca40b
AyeChanHtunNaing <ayechanhtunnaing@icloud.com>
2024-04-20 00:01:05 +0630

    feat: sec component creation

--- react-todo-demo/react-todos/src/App.js
+++ react-todo-demo/react-todos/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import TodoRowItem from './components/TodoRowItem';
+
+import TodoTable from './components/TodoTable';
 
 function App() {
   const todos = [
@@ -21,32 +22,7 @@ function App() {
           Your Todo's
         </div>
         <div className='card-body'>
-          <table className='table table-hover'>
-            <thead>
-              <tr>
-                <th scope='col'>#</th>
-                <th scope='col'>Description</th>
-                <th scope='col'>Assigned</th>
-              </tr>
-            </thead>
-            <tbody>
-              <TodoRowItem
-                rowNumber={todos[0].rowNumber}
-                rowDescription={todos[0].rowDescription}
-                rowAssigned={todos[0].rowAssigned}
-              />
-              <TodoRowItem
-                rowNumber={todos[1].rowNumber}
-                rowDescription={todos[1].rowDescription}
-                rowAssigned={todos[1].rowAssigned}
-              />
-              <TodoRowItem
-                rowNumber={todos[2].rowNumber}
-                rowDescription={todos[2].rowDescription}
-                rowAssigned={todos[2].rowAssigned}
-              />
-            </tbody>
-          </table>
+          <TodoTable todos={todos} />
         </div>
       </div>
     </div >
