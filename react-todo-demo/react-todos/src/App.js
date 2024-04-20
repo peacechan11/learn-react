09675130c203b53c4fb51b0b7cb57474f2f17a14
AyeChanHtunNaing <ayechanhtunnaing@icloud.com>
2024-04-20 13:28:07 +0630

    feat : add button

--- react-todo-demo/react-todos/src/App.js
+++ react-todo-demo/react-todos/src/App.js
@@ -18,6 +18,18 @@ function App() {
       rowNumber: 4, rowDescription: 'System Analysis', rowAssigned: 'Pika'
     }
   ]
+  const addTodo = () => {
+    if (todos.length > 0) {
+      const newTodo = {
+        rowNumber: todos.length + 1,
+        rowDescription: 'New Todo',
+        rowAssigned: 'User Three'
+      }
+      todos.push(newTodo);
+      console.log(todos);
+    }
+  }
+
   return (
     <div className='mt-5 container'>
       <div className='card'>
@@ -26,6 +38,9 @@ function App() {
         </div>
         <div className='card-body'>
           <TodoTable todos={todos} />
+          <button className="btn btn-primary" onClick={addTodo}>
+            Add new todo
+          </button>
         </div>
       </div>
     </div >
